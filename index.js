b87689ea9262c6dedaec0585164e2ade0090d1ea
yblair <lair.yamila.belen@gmail.com>
2022-10-16 17:48:57 -0300

    index changes

--- index.js
+++ index.js
@@ -18,9 +18,9 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const PORT =  process.env.PORT || 3001
+
 // Syncing all the models at once.
 
-  server.listen(PORT, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
